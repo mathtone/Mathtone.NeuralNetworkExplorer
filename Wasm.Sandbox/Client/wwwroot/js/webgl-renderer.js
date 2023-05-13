4697b5f531bfdf7a61f1cbef7092e1debc069445
mik3c <mike.carleton@gmail.com>
2023-05-13 17:02:27 -0400

    wasm stuff.

---
+++ Wasm.Sandbox/Client/wwwroot/js/webgl-renderer.js
@@ -0,0 +1,101 @@
+﻿var WebGLRenderer = (function () {
+   var gl;
+   var program;
+   var vertexPositionAttribute;
+   var vertexColorAttribute;
+   var vertexBuffer;
+   var colorBuffer;
+
+   function initRenderer(webglContext) {
+      gl = webglContext;
+
+      // Create the vertex and fragment shaders
+      var vertexShaderSource = `
+        // Vertex shader code
+        attribute vec4 aVertexPosition;
+        attribute vec4 aVertexColor;
+        varying lowp vec4 vColor;
+        void main() {
+            gl_Position = aVertexPosition;
+            vColor = aVertexColor;
+        }
+    `;
+
+      var fragmentShaderSource = `
+        // Fragment shader code
+        varying lowp vec4 vColor;
+        void main() {
+            gl_FragColor = vColor;
+        }
+    `;
+
+      var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+      var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+      program = createProgram(gl, vertexShader, fragmentShader);
+      gl.useProgram(program);
+
+      // Get attribute locations
+      vertexPositionAttribute = gl.getAttribLocation(program, 'aVertexPosition');
+      vertexColorAttribute = gl.getAttribLocation(program, 'aVertexColor');
+
+      // Create buffers
+      vertexBuffer = gl.createBuffer();
+      colorBuffer = gl.createBuffer();
+
+      // Set up the canvas
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+      gl.clearColor(0, 0, 0, 1);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+   }
+
+   function renderBitmap(bitmapData) {
+      // Rendering logic using WebGL
+      // ...
+   }
+
+   function createShader(gl, type, source) {
+      // Create and compile the shader
+      var shader = gl.createShader(type);
+      gl.shaderSource(shader, source);
+      gl.compileShader(shader);
+
+      // Check for shader compilation errors
+      var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+      if (!success) {
+         var infoLog = gl.getShaderInfoLog(shader);
+         console.error('Shader compilation error:', infoLog);
+         gl.deleteShader(shader);
+         return null;
+      }
+
+      return shader;
+   }
+
+   function createProgram(gl, vertexShader, fragmentShader) {
+      // Create the program
+      var program = gl.createProgram();
+      gl.attachShader(program, vertexShader);
+      gl.attachShader(program, fragmentShader);
+      gl.linkProgram(program);
+
+      // Check for program linking errors
+      var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+      if (!success) {
+         var infoLog = gl.getProgramInfoLog(program);
+         console.error('Program linking error:', infoLog);
+         gl.deleteProgram(program);
+         return null;
+      }
+
+      return program;
+   }
+
+   return {
+      initRenderer: initRenderer,
+      renderBitmap: renderBitmap,
+   };
+})();
+
+// Export the WebGLRenderer object
+export default WebGLRenderer;
