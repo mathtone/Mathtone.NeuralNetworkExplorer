9b5d0f3f54268a23166f82bb5ad541752b31977b
mik3c <mike.carleton@gmail.com>
2026-06-11 05:49:27 -0400

    Mik3c wasm (#2)
    * wasm stuff.
    
    * Upgrade to .NET 8.0 and modernize project structure
    
    Updated project files for Mathtone Neural Network Explorer to target .NET 8.0, replacing .NET 7.0. Significant changes in `Mathtone.NeuralNetworkExplorer.csproj`, including a shift to the new SDK style and targeting .NET Framework 4.7.2. Removed outdated references and added new package references. Enhanced project structure for better alignment with current .NET development practices.

---
+++ Mathtone.NeuralNetworkExplorer.Wasm/wwwroot/canvasInterop.js
@@ -0,0 +1,20 @@
+﻿// canvasInterop.js
+
+window.canvasInterop = {
+   drawBitmap: function (canvasId, base64Image) {
+      const canvas = document.getElementById(canvasId);
+      const context = canvas.getContext('2d');
+
+      // Clear the canvas
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Create an image object
+      const image = new Image();
+      image.src = "data:image/png;base64," + base64Image;
+
+      // Draw the image onto the canvas
+      image.onload = function () {
+         context.drawImage(image, 0, 0);
+      };
+   }
+};
